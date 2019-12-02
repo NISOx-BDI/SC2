50ede71d0581af2a78156235ecea56fa06cda51e
Alexander Bowring <alex.bowring@bdi.ox.ac.uk>
2019-12-02 17:43:37 +0000

    ds001 subject level scripts

---
+++ data/processed/ds001/scripts/ds001_sub-06_fmriprep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -N ds001_sub-06
+#$ -q long.qc
+#$ -o /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds001/logs/
+#$ -e /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds001/logs/
+
+singularity run --cleanenv -B /well/win/software/packages,/well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds001_R2.0.4/../../.. /well/nichols/users/bas627/fmriprep/fmriprep-1.5.1.simg \
+    /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds001_R2.0.4 /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds001 \
+    participant \
+    --participant-label 06 \
+    --ignore slicetiming \
+    --fs-license-file /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/license.txt \
+    --fs-no-reconall \
+    -w /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds001/ds001_sub-06_work \
+    --mem-mb 13000 \
+    --low-mem \
+    --resource-monitor \
+    --nthreads 4 \
+    -vvv
+
