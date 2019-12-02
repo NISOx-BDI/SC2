36c9bd88fa8c566bf1bdcd08e1add3d215885e00
Alexander Bowring <alex.bowring@bdi.ox.ac.uk>
2019-12-02 14:15:27 +0000

    more run_fmriprep

--- src/lib/run_fmriprep.py
+++ src/lib/run_fmriprep.py
@@ -15,18 +15,19 @@ def run_fmriprep(raw_dir, out_dir, template_script, packages_dir, FS_license):
 
 	# Obtain the list of subjects from the raw data directory
 	sub_dirs = glob.glob(os.path.join(raw_dir, 'sub-*'))
-	subs     = [os.path.basename(w) for w in sub_dirs]
-	print(subs)
+	subs     = [os.path.basename(w) for w in sub_dirs
 
 	# Creating and running an fmriprep script for each subjects
 	for s in subs:
 		# New dict for each subject
-		values = dict()
+		values 		       = dict()
 		values["packages_dir"] = packages_dir
 		values["FS_license"]   = FS_license
-		values["out_dir"]	   = out_dir
-		sub_reg = re.search('sub-\d+', s)
-		print(sub_reg)
-		sub = subreg.group(0)
-		values["sub"] = sub
-		print(sub)
+		values["out_dir"]      = out_dir
+		sub_reg 	       = re.search('sub-\d+', s)
+		sub 	               = sub_reg.group(0)
+		values["sub"] 	       = sub
+		sub_id 		       = sub.split("-")[1]
+		values["sub_id"]       = sub_id
+		study  	               = os.path.basename(out_dir)
+		print(study) 
