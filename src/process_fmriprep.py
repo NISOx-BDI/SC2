d1f3d0f0197556ee4b2c391d74c80e4abe196265
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2019-12-02 13:43:33 +0000

    cleaning up master fmriprep script

--- src/process_fmriprep.py
+++ src/process_fmriprep.py
@@ -1,6 +1,6 @@
 # Preprocessing of ds001, ds109 and ds120 with fmriprep
 import os
-from lib
+from lib.run_fmriprep import run_fmriprep
 
 # Specifiy the directory containing all the packages needed for fmriprep
 packages_dir = '/well/win/software/packages'
@@ -15,17 +15,17 @@ ds001_raw_dir = os.path.join(raw_data_dir,'ds001_R2.0.4')
 ds109_raw_dir = os.path.join(raw_data_dir,'ds000109_R2.0.1')
 ds120_raw_dir = os.path.join(raw_data_dir,'ds120_R1.0.0')
 
-# Output directories for preprocessed ds001, ds109, and ds120
-processed_data_dir  = os.path.join(home_dir,'data','processed')
-ds001_processed_dir = os.path.join(processed_data_dir,'ds001')
-ds109_processed_dir = os.path.join(processed_data_dir,'ds109')
-ds120_processed_dir = os.path.join(processed_data_dir,'ds120')
-
 # Template subject-level processing scripts
 scripts_dir             = os.path.join(home_dir,'src')
 ds001_fmriprep_template = os.path.join(scripts_dir,'ds001','ds001_fmriprep_template')
 ds109_fmriprep_template = os.path.join(scripts_dir,'ds109','ds109_fmriprep_template')
 ds120_fmriprep_template = os.path.join(scripts_dir,'ds120','ds120_fmriprep_template')
+
+# Output directories for preprocessed ds001, ds109, and ds120
+processed_data_dir  = os.path.join(home_dir,'data','processed')
+ds001_processed_dir = os.path.join(processed_data_dir,'ds001')
+ds109_processed_dir = os.path.join(processed_data_dir,'ds109')
+ds120_processed_dir = os.path.join(processed_data_dir,'ds120')
 
 # Run fmriprep on all ds001 subjects
 run_fmriprep(ds001_raw_dir, ds001_processed_dir, ds001_fmriprep_template, packages_dir, FS_license)
