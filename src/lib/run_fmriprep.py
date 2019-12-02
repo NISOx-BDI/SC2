160e46367bc4297e719989e46cfd2b2cba3fb9b2
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2019-12-02 14:02:11 +0000

    run_fmriprep progress

--- src/lib/run_fmriprep.py
+++ src/lib/run_fmriprep.py
@@ -1,13 +1,13 @@
-import os, glob 
+import os, glob, re
 
-def run_fmriprep(raw_dir, output_dir, template_script, packages_dir, FS_license):
+def run_fmriprep(raw_dir, out_dir, template_script, packages_dir, FS_license):
 	
 	# Make the directory where all fmriprep scripts and outputs will be stored
-	if not os.path.isdir(output_dir):
-    		os.mkdir(output_dir)
+	if not os.path.isdir(out_dir):
+    		os.mkdir(out_dir)
 
 	# Directory where subject-level fmriprep scripts will be made
-	scripts_dir = os.path.join(output_dir, os.pardir, 'scripts')
+	scripts_dir = os.path.join(out_dir, os.pardir, 'scripts')
 
 	if not os.path.isdir(scripts_dir):
 		os.mkdir(scripts_dir)
@@ -17,3 +17,15 @@ def run_fmriprep(raw_dir, output_dir, template_script, packages_dir, FS_license)
 	sub_dirs = glob.glob(os.path.join(raw_dir, 'sub-*'))
 	subs     = [os.path.basename(w) for w in sub_dirs]
 	print(subs)
+
+	# Creating and running an fmriprep script for each subjects
+	for s in subs:
+		# New dict for each subject
+		values = dict()
+		values["packages_dir"] = packages_dir
+		values["FS_license"]   = FS_license
+		values["out_dir"]	   = out_dir
+		sub_reg = re.search('sub-\d+', s)
+		sub = subreg.group(0)
+        values["sub"] = sub
+        print(sub)
