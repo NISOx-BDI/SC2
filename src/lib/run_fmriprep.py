d4346cb531f24cefb06ea0db9d70eaa0301f6670
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2019-12-03 10:49:33 +0000

    adding feature to delete files for failed fmriprep runs and re-run

--- src/lib/run_fmriprep.py
+++ src/lib/run_fmriprep.py
@@ -59,6 +59,15 @@ def run_fmriprep(raw_dir, out_dir, template_script, packages_dir, fmriprep_singu
 		values["sub"] = sub
 		sub_id = sub.split("-")[1]
 		values["sub_id"] = sub_id
+
+		# If fmriprep failed last time (i.e. the summary report sub-??.html was not created), then we delete all subject's fmriprep files and re-run
+		if not os.path.isfile(os.path.join(out_dir, fmriprep, sub + '.html')):
+			print(study + sub + ' fail')
+			#shutil.rmtree(os.path.join(out_dir, fmriprep, sub))
+			#shutil.rmtree(os.path.join(out_dir, study + '_' + sub + '_work'))
+			#os.remove(os.path.join(scripts_dir, study + '_' + sub + '_fmriprep.sh'))
+		else:
+			print(study + sub + ' success')
 		
 		if not os.path.isfile(os.path.join(scripts_dir, study + '_' + sub + '_fmriprep.sh')):
 			# Fill-in the subject-level fmriprep template
