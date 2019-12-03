61ed653156727cc0f6060fafbab8213ad48c4a9d
Alexander Bowring <alex.bowring@bdi.ox.ac.uk>
2019-12-03 10:40:48 +0000

    ds109 subject level scripts

---
+++ data/processed/ds109/scripts/ds109_sub-03_fmriprep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -N ds109_sub-03
+#$ -q long.qc
+#$ -o /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds109/logs/
+#$ -e /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds109/logs/
+
+singularity run --cleanenv -B /well/win/software/packages,/well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds000109_R2.0.1/../../.. /well/nichols/users/bas627/fmriprep/fmriprep-1.5.1.simg \
+    /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds000109_R2.0.1 /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds109 \
+    participant \
+    --participant-label 03 \
+    --ignore slicetiming \
+    --fs-license-file /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/license.txt \
+    --fs-no-reconall \
+    -w /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds109/ds109_sub-03_work \
+    --mem-mb 13000 \
+    --low-mem \
+    --resource-monitor \
+    --nthreads 4 \
+    -vvv
+
---
+++ data/processed/ds109/scripts/ds109_sub-18_fmriprep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -N ds109_sub-18
+#$ -q long.qc
+#$ -o /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds109/logs/
+#$ -e /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds109/logs/
+
+singularity run --cleanenv -B /well/win/software/packages,/well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds000109_R2.0.1/../../.. /well/nichols/users/bas627/fmriprep/fmriprep-1.5.1.simg \
+    /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds000109_R2.0.1 /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds109 \
+    participant \
+    --participant-label 18 \
+    --ignore slicetiming \
+    --fs-license-file /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/license.txt \
+    --fs-no-reconall \
+    -w /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds109/ds109_sub-18_work \
+    --mem-mb 13000 \
+    --low-mem \
+    --resource-monitor \
+    --nthreads 4 \
+    -vvv
+
