06dcf3dcda11293bdd1b32c14c43842fab772449
Alexander Bowring <alex.bowring@bdi.ox.ac.uk>
2019-12-12 15:05:30 +0000

    added config.py that will store all absolute paths

---
+++ src/config.py
@@ -0,0 +1,6 @@
+import os
+paths = dict()
+paths["packages_dir"] = '/well/win/software/packages'
+paths["home_dir"] = '/well/nichols/users/bas627/BIDS_Data/RESULTS/SC2'
+paths["fmriprep_singularity_image"] = '/well/nichols/users/bas627/fmriprep/fmriprep-1.5.1.simg'
+paths["FS_license"] = os.path.join(paths.get("home_dir"),'license.txt')
