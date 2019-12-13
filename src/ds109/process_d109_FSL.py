89173bc186df98e0010060156c2d1695b5b613f3
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2019-12-13 16:45:59 +0000

    working on FSL ds109 onsets + 1st level

--- src/ds109/process_d109_FSL.py
+++ src/ds109/process_d109_FSL.py
@@ -7,10 +7,10 @@ from lib.fsl_processing import create_fsl_onset_files, run_run_level_analyses
 
 locals().update(paths)
 
-ds001_raw_dir = os.path.join(home_dir,'data','raw','ds001_R2.0.4')
-ds001_processed_dir = os.path.join(home_dir,'data','processed','ds001')
-fmriprep_dir = os.path.join(ds001_processed_dir,'fmriprep')
-fsl_dir = os.path.join(home_dir,'results','ds001','FSL')
+ds109_raw_dir = os.path.join(home_dir,'data','raw','ds000109_R2.0.1')
+ds109_processed_dir = os.path.join(home_dir,'data','processed','ds109')
+fmriprep_dir = os.path.join(ds109_processed_dir,'fmriprep')
+fsl_dir = os.path.join(home_dir,'results','ds109','FSL')
 
 if not os.path.isdir(fsl_dir):
     os.mkdir(fsl_dir)
@@ -21,35 +21,28 @@ level3_dir = os.path.join(fsl_dir, 'LEVEL2', 'group')
 perm_dir = os.path.join(fsl_dir, 'LEVEL2', 'permutation_test')
 mni_dir = os.path.join(fsl_dir, 'mean_mni_images')
 
-# Specify the number of functional volumes ignored in the study
-TR = 2
-num_ignored_volumes = 2
-
-# Specify the TR that will be removed from onesets, equal to num_ignored_volumes*TR
-removed_TR_time = num_ignored_volumes*TR 
+# Specify the subjects of interest from the raw data
+subject_ids = [1, 2, 3, 8, 9, 10, 11, 14, 15, 17, 18, 21, 22, 26, 27, 28, 30, 31, 32, 43, 48]
+subject_ids = ['{num:02d}'.format(num=x) for x in subject_ids]
 
 # Directory to store the onset files
 onsetDir = os.path.join(fsl_dir, 'ONSETS')
 
 # Define conditions and parametric modulations (if any)
 conditions = (
-    (('pumps_fixed', 'pumps_demean'), ('pumps_demean',)),
-    ('pumps_RT', ('pumps_demean', 'response_time')),
-    (('cash_fixed', 'cash_demean'), ('cash_demean',)),
-    ('cash_RT', ('cash_demean', 'response_time')),
-    (('explode_fixed', 'explode_demean'), ('explode_demean',)),
-    (('control_pumps_fixed', 'control_pumps_demean'),
-     ('control_pumps_demean',)),
-    ('control_pumps_RT', ('control_pumps_demean', 'response_time')))
+    ('false_belief_story', ('false belief story', 'duration')),
+    ('false_belief_question', ('false belief question', 'duration')),
+    ('false_photo_story', ('false photo story', 'duration')),
+    ('false_photo_question', ('false photo question', 'duration')))
 
 # Create 3-columns onset files based on BIDS tsv files
-cond_files = create_fsl_onset_files(ds001_raw_dir, onsets_dir, conditions, removed_TR_time)
+cond_files = create_fsl_onset_files(ds109_raw_dir, onsets_dir, conditions, removed_TR_time)
 
 cwd = os.path.dirname(os.path.realpath(__file__))
-run_level_fsf = os.path.join(cwd,'template_ds001_FSL_level1.fsf')
+run_level_fsf = os.path.join(cwd,'template_ds109_FSL_level1.fsf')
 
 # Run a GLM for each fMRI run of each subject
-run_run_level_analyses(fmriprep_dir, run_level_fsf, level1_dir, cond_files)
+#run_run_level_analyses(fmriprep_dir, run_level_fsf, level1_dir, cond_files)
 
 # Run a GLM combining all the fMRI runs of each subject
 #run_subject_level_analyses(level1_dir, sub_level_fsf, level2_dir)
