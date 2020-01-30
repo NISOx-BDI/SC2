3e57262dd6ee21cd930ae1215d7fa4e421d496c9
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2020-01-30 15:07:05 +0000

    getting onsets for ds001 SPM

--- src/ds001/process_ds001_SPM.m
+++ src/ds001/process_ds001_SPM.m
@@ -1,12 +1,16 @@
-study_dir = '/home/maullz/NIDM-Ex/BIDS_Data/DATA/BIDS/ds001_R2.0.4';
-results_dir = '/home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/ds001';
+[config_dir,~,~] = fileparts(pwd);
+load(fullfile(config_dir,'config.mat'))
 
-spm_dir = fullfile(results_dir, 'SPM');
+ds001_raw_dir = fullfile(home_dir,'data','raw','ds001_R2.0.4');
+ds001_processed_dir = fullfile(home_dir,'data','processed','ds001');
+fmriprep_dir = fullfile(ds001_processed_dir,'fmriprep');
+spm_dir = fullfile(home_dir,'results','ds001','SPM');
+
 if ~isdir(spm_dir)
     mkdir(spm_dir) 
 end
 
-preproc_dir = fullfile(spm_dir, 'PREPROCESSING');
+onsets_dir = fullfile(spm_dir, 'ONSETS');
 level1_dir = fullfile(spm_dir, 'LEVEL1');
 level2_dir = fullfile(spm_dir, 'LEVEL2');
 perm_dir = fullfile(level2_dir, 'permutation_test');
@@ -18,17 +22,6 @@ num_ignored_volumes = 2;
 
 % Specify the TR that will be removed from onsets, equal to num_ignored_volumes*TR
 removed_TR_time = num_ignored_volumes*TR;
-
-% Add 'lib' folder to the matlab path
-if ~exist('copy_gunzip', 'file')
-    addpath(fullfile(fileparts(mfilename('fullpath')), 'lib'))
-end
-
-copy_gunzip(study_dir, preproc_dir);
-
-% Directory to store the onset files
-onsetDir = fullfile(spm_dir,'ONSETS');
-
 
 % Define conditions and parametric modulations (if any)
 % FORMAT
@@ -45,8 +38,8 @@ CondNames = {...
     {'control_pumps_RT', {'control_pumps_demean', 'response_time'}}};
 
 create_onset_files(study_dir, onsetDir, CondNames, removed_TR_time);
-spm('defaults','FMRI');
-run_subject_level_analyses(study_dir, preproc_dir, 'template_ds001_SPM_level1', level1_dir, num_ignored_volumes, TR);
-run_group_level_analysis(level1_dir, 'template_ds001_SPM_level2', level2_dir, '0001');
-run_permutation_test(level1_dir, 'template_ds001_SPM_perm_test', perm_dir, '0001');
-mean_mni_images(preproc_dir, level1_dir, mni_dir);
+%spm('defaults','FMRI');
+%run_subject_level_analyses(study_dir, preproc_dir, 'template_ds001_SPM_level1', level1_dir, num_ignored_volumes, TR);
+%run_group_level_analysis(level1_dir, 'template_ds001_SPM_level2', level2_dir, '0001');
+%run_permutation_test(level1_dir, 'template_ds001_SPM_perm_test', perm_dir, '0001');
+%mean_mni_images(preproc_dir, level1_dir, mni_dir);
