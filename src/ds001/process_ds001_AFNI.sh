9397da95e49183bd01dc14060ed90b31790df031
Alexander Bowring <alex.bowring@bdi.ox.ac.uk>
2020-03-09 15:33:39 +0000

    updated file path

--- src/ds001/process_ds001_AFNI.sh
+++ src/ds001/process_ds001_AFNI.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-#$ -N run_AFNI
+#$ -N run_AFNI_ds001
 #$ -q long.qc
-#$ -o /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/results/AFNI/ds001/logs
-#$ -e /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/results/AFNI/ds001/logs
+#$ -o /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/results/ds001/AFNI/logs/
+#$ -e /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/results/ds001/AFNI/logs/
 
-python /home/maullz/NIDM-Ex/BIDS_Data/RESULTS/SOFTWARE_COMPARISON/scripts/process_ds001_AFNI.py
+. /etc/profile
+. ~/.bash_profile
+
+cd /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/src/ds001
+python process_ds001_AFNI.py
