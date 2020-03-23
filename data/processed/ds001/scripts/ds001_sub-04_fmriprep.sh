a426dc618f586ac869359c9561d40b8c8f9fc476
Alexander Bowring <alex.bowring@bdi.ox.ac.uk>
2020-03-23 14:06:34 +0000

    reprocessed dsata for ds001 and ds120

--- data/processed/ds001/scripts/ds001_sub-04_fmriprep.sh
+++ data/processed/ds001/scripts/ds001_sub-04_fmriprep.sh
@@ -4,17 +4,16 @@
 #$ -o /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds001/logs/
 #$ -e /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds001/logs/
 
-singularity run --cleanenv -B /well/win/software/packages,/well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds001_R2.0.4/../../.. /well/nichols/users/bas627/fmriprep/fmriprep-1.5.1.simg \
-    /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds001_R2.0.4 /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds001 \
+singularity run -B /well/win/software/packages,/well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds001_R2.0.4_AMENDED/../../.. --home /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds001_R2.0.4_AMENDED/../../.. --cleanenv /well/nichols/users/bas627/fmriprep/fmriprep-20.0.2.simg \
+    /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds001_R2.0.4_AMENDED /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds001 \
     participant \
     --participant-label 04 \
     --ignore slicetiming \
     --fs-license-file /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/license.txt \
-    --fs-no-reconall \
     -w /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds001/ds001_sub-04_work \
-    --mem-mb 13000 \
+    --mem-mb 15000 \
     --low-mem \
     --resource-monitor \
     --nthreads 4 \
-    -vvv
+    -vvv 
 
--- data/processed/ds120/scripts/ds120_sub-26_fmriprep.sh
+++ data/processed/ds120/scripts/ds120_sub-26_fmriprep.sh
@@ -4,17 +4,16 @@
 #$ -o /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds120/logs/
 #$ -e /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds120/logs/
 
-singularity run --cleanenv -B /well/win/software/packages,/well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds120_R1.0.0/../../.. /well/nichols/users/bas627/fmriprep/fmriprep-1.5.1.simg \
+singularity run -B /well/win/software/packages,/well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds120_R1.0.0/../../.. --home /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds120_R1.0.0/../../.. --cleanenv /well/nichols/users/bas627/fmriprep/fmriprep-20.0.2.simg \
     /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/raw/ds120_R1.0.0 /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds120 \
     participant \
     --participant-label 26 \
     --ignore slicetiming \
     --fs-license-file /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/license.txt \
-    --fs-no-reconall \
     -w /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/data/processed/ds120/ds120_sub-26_work \
-    --mem-mb 13000 \
+    --mem-mb 15000 \
     --low-mem \
     --resource-monitor \
     --nthreads 4 \
-    -vvv
+    -vvv 
 
