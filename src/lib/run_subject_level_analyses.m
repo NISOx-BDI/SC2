f621e3f375600ed273ddff6cb51d45abe9e54306
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2020-03-24 15:12:53 +0000

    Updated so that it will continue with subjects if not all were completed

--- src/lib/run_subject_level_analyses.m
+++ src/lib/run_subject_level_analyses.m
@@ -22,22 +22,34 @@ function run_subject_level_analyses(fmriprep_dir, sub_template, level1_dir, num_
         OUT_DIR = fullfile(level1_dir, sub);
         sub = ['^' sub];
         
-        fmri_files = cellstr(spm_select('List', func_dir, [sub '.*\-preproc_bold.nii']));
-        for r = 1:numel(fmri_files)
-            sub_run = [sub '.*_run-' sprintf('%02d',r)];
-            fmris = cellstr(spm_select('ExtFPList', func_dir, [sub_run '.*\-preproc_bold.nii'], Inf)); 
-            fmris = fmris(num_ignored_volumes+1:end);
-            eval(['FUNC_RUN_' num2str(r) ' =  fmris;']);
-            onset_file = spm_select('FPList', onsets_dir, [sub_run '.*\.mat']);
-            eval(['ONSETS_RUN_' num2str(r) ' = onset_file;']);
-            motion_regressor_file = spm_select('FPList', motion_regressors_dir, [sub_run '.*\.mat']);
-            eval(['MOTION_REGRESSORS_RUN_' num2str(r) ' = motion_regressor_file;']);
+        % If subject did not complete from a previous attempt, removes all results files and runs again
+        if ~isfile(fullfile(OUT_DIR,'spmT_0001.nii'))
+            
+            if isdir(OUT_DIR)
+                rmdir(OUT_DIR)
+            end
+            
+            if isfile(fullfile(scripts_dir, [strrep(sub,'^','') '_level1.mat']))
+                delete(fullfile(scripts_dir, [strrep(sub,'^','') '_level1.mat']))
+            end
+
+            fmri_files = cellstr(spm_select('List', func_dir, [sub '.*\-preproc_bold.nii']));
+            for r = 1:numel(fmri_files)
+                sub_run = [sub '.*_run-' sprintf('%02d',r)];
+                fmris = cellstr(spm_select('ExtFPList', func_dir, [sub_run '.*\-preproc_bold.nii'], Inf)); 
+                fmris = fmris(num_ignored_volumes+1:end);
+                eval(['FUNC_RUN_' num2str(r) ' =  fmris;']);
+                onset_file = spm_select('FPList', onsets_dir, [sub_run '.*\.mat']);
+                eval(['ONSETS_RUN_' num2str(r) ' = onset_file;']);
+                motion_regressor_file = spm_select('FPList', motion_regressors_dir, [sub_run '.*\.mat']);
+                eval(['MOTION_REGRESSORS_RUN_' num2str(r) ' = motion_regressor_file;']);
+            end
+            
+            % Create the matlabbatch for this subject
+            eval(sub_template);
+            
+            save(fullfile(scripts_dir, [strrep(sub,'^','') '_level1.mat']), 'matlabbatch');
+            spm_jobman('run', matlabbatch);
         end
-        
-        % Create the matlabbatch for this subject
-        eval(sub_template);
-        
-        save(fullfile(scripts_dir, [strrep(sub,'^','') '_level1.mat']), 'matlabbatch');
-        spm_jobman('run', matlabbatch);
     end
 end
