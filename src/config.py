cd67f19f71301a7a09cc1b34fd4da155fc46068c
Alexander Bowring <alex.bowring@bdi.ox.ac.uk>
2020-03-30 00:05:18 +0100

    config.py calls updated AFNI container

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 paths = dict()
 paths["packages_dir"] = '/well/win/software/packages'
 paths["home_dir"] = '/well/nichols/users/bas627/BIDS_Data/RESULTS/SC2'
-paths["fmriprep_singularity_image"] = '/well/nichols/users/bas627/fmriprep/fmriprep-1.5.1.simg'
+paths["fmriprep_singularity_image"] = '/well/nichols/users/bas627/fmriprep/fmriprep-20.0.2.simg'
 paths["FS_license"] = os.path.join(paths.get("home_dir"),'license.txt')
-paths["AFNI_SPM_singularity_image"] = '/apps/singularity/fsl-afni-spm12-conda.sif'
+paths["AFNI_SPM_singularity_image"] = '/apps/singularity/afni-r-python3-2020-03-26-v1.sif'
 paths["AFNI_bin"] = '/opt/afni-latest'
