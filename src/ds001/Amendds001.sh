d76cb360a00689b7b8485dccf69be374a3cac6df
Alexander Bowring <alex.bowring@bdi.ox.ac.uk>
2020-03-30 00:13:59 +0100

    More masking of sub-04 to help with preprocessing

--- src/ds001/Amendds001.sh
+++ src/ds001/Amendds001.sh
@@ -26,5 +26,6 @@ fslmaths $sub04_anat_dir/sub-04_T1w.nii.gz -roi 0 -1 0 -1 107 -1 0 1 $sub04_anat
 fslmaths $sub04_anat_dir/sub-04_T1w.nii.gz -roi 70 21 112 -1 0 -1 0 1 $sub04_anat_dir/sub-04_T1w_between_eyes.nii.gz
 fslmaths $sub04_anat_dir/sub-04_T1w.nii.gz -roi 0 -1 0 70 0 -1 0 1 $sub04_anat_dir/sub-04_T1w_back_of_head.nii.gz
 fslmaths $sub04_anat_dir/sub-04_T1w_brain.nii.gz -add $sub04_anat_dir/sub-04_T1w_top_of_head.nii.gz -add $sub04_anat_dir/sub-04_T1w_between_eyes.nii.gz -add $sub04_anat_dir/sub-04_T1w_back_of_head.nii.gz -bin -mul $sub04_anat_dir/sub-04_T1w.nii.gz $sub04_anat_dir/sub-04_T1w.nii.gz
-
+fslmaths $sub04_anat_dir/sub-04_T1w.nii.gz -thr 400 -bin -mul 200 $sub04_anat_dir/sub-04_T1w_above_420.nii.gz
+fslmaths $sub04_anat_dir/sub-04_T1w.nii.gz -uthr 399 -add $sub04_anat_dir/sub-04_T1w_above_420.nii.gz $sub04_anat_dir/sub-04_T1w.nii.gz
 rm -rf $sub04_anat_dir/sub-04_T1w_*
