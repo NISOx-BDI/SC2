e80d94a4092779a25c51de931c4f63796a5e0e6e
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2020-04-16 13:46:21 +0100

    copy_unzip_func now copies/unzips the fmriprep'd mask files too

--- src/lib/copy_unzip_func.m
+++ src/lib/copy_unzip_func.m
@@ -8,7 +8,7 @@ function copy_unzip_func(fmriprep_dir, spm_dir)
 
     sub_dirs = cellstr(spm_select('FPList',fmriprep_dir, 'dir','sub-*'));
     
-    % Copying all fmriprep functional files to a new directory and unzipping
+    % Copying all fmriprep functional files and mask files to a new directory and unzipping
     matlabbatch = cell(0);
     for i = 1:numel(sub_dirs)
         [~,sub,~] = fileparts(sub_dirs{i}); 
@@ -21,7 +21,17 @@ function copy_unzip_func(fmriprep_dir, spm_dir)
             matlabbatch{end+1}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.files(1) = func_files(r);
             matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.outdir = {func_dir};
             matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.keep = false;  
-        end    
+        end
+        mask_files = cellstr(spm_select('FPList', fmriprep_func_dir, ['.*-brain_mask.nii.gz']));
+        for k = 1:numel(mask_files)
+            matlabbatch{end+1}.cfg_basicio.file_dir.file_ops.file_move.files = {mask_files{k}};
+            matlabbatch{end}.cfg_basicio.file_dir.file_ops.file_move.action.copyto = {func_dir};
+            func_files{r} = spm_file(mask_files{k}, 'path', func_dir);
+            matlabbatch{end+1}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.files(1) = func_files(k);
+            matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.outdir = {func_dir};
+            matlabbatch{end}.cfg_basicio.file_dir.file_ops.cfg_gunzip_files.keep = false;  
+        end
+
     end
     spm_jobman('run', matlabbatch);
 end
