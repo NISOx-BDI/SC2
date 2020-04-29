de8513e8e57a05fd636a3fae42aa054d12e42ab2
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2020-04-30 00:22:18 +0100

    debug, function for fsl analyses with spm design and drift model

--- src/lib/fsl_processing.py
+++ src/lib/fsl_processing.py
@@ -635,7 +635,7 @@ def run_run_level_spm_drift(fmriprep_dir, run_level_fsf, level1_dir, spm_design_
 
             spm_drift_files = sorted(glob.glob(os.path.join(spm_design_dir,'spm_drift*.txt')))
             for j, drift_file in enumerate(spm_drift_files):
-                vales['spm_drift_' + str(j+1)] = drift_file
+                values['spm_drift_' + str(j+1)] = drift_file
         
             # Fill-in the template run-level design.fsf
             with open(run_level_fsf) as f:
