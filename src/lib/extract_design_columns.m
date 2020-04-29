ec1c22690849fba1980b53238f96bd3dad10d435
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2020-04-29 16:53:17 +0100

    debug, script was getting the wrong columns for 2nd and 3rd runs

--- src/lib/extract_design_columns.m
+++ src/lib/extract_design_columns.m
@@ -17,15 +17,16 @@ function extract_design_columns(level1_dir, design_dir)
         fmri_files = cellstr(spm_select('List', func_dir, [sub '.*\-preproc_bold.nii']));
         nruns = length(fmri_files);
 
-        % Obtaining the number of run-level regressors (excluding the 6 motion regressors) in the design matrix for the subject
-        nregressors = ((length(SPM.SPM.xX.name) - nruns)/nruns) - 6;
+        % Obtaining the number of run-level regressors in the design matrix for the subject
+        nregressors = ((length(SPM.SPM.xX.name) - nruns)/nruns);
 
         % Obtaining the number of time-points in each run
         ntimepoints = size(SPM.SPM.xX.X, 1)/nruns;
 
         % For each run, extract all the run-level regressors in the design matrix to a text file 
         for r = 1:nruns
-            for q = 1:nregressors
+            % Exclude the motion regressors 
+            for q = 1:(nregressors-6)
                 column = SPM.SPM.xX.X((1+ntimepoints*(r-1)):ntimepoints*r,q+nregressors*(r-1));
                 % writing the column to a text file
                 output_filename = fullfile(design_dir, [strrep(sub,'^','') '_run-' sprintf('%02d',r) '_regressor_' sprintf('%02d', q) '.txt']);
