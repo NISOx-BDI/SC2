fc822e55812cd57bf1a8dd254f68671e851e8f32
Alexander Bowring <alex.bowring@bdi.ox.ac.uk>
2020-06-12 18:14:48 +0100

    debugged, files have correct output name

--- src/lib/make_varcopes.m
+++ src/lib/make_varcopes.m
@@ -1,6 +1,6 @@
 function make_varcopes(level1_dir)
 
-	out_dir = fullfile(level1_dir, 'subject_varcopes')
+	out_dir = fullfile(level1_dir, 'varcopes')
 
 	if ~isdir(out_dir)
 		mkdir(out_dir)
@@ -11,14 +11,14 @@ function make_varcopes(level1_dir)
 	for i = 1:numel(sub_dirs)
 
 		[~,sub,~] = fileparts(sub_dirs{i});
-		subject_mat_file = fullfile(level1_dir, sub, 'SPM.mat')
-		out_name = fullfile()
+		subject_mat_file = fullfile(sub_dirs{i}, 'SPM.mat');
 
 		ic = 1; % select the contrast index
-		load subject_mat_file
+		load(subject_mat_file)
 		disp(sprintf('Selecting con %d "%s"\n', ic, SPM.xCon(ic).name))
 		vsca = SPM.xCon(ic).c'*SPM.xX.Bcov*SPM.xCon(ic).c;
-		varfn = fullfile(out_dir, sprintf('convar_%04d.nii', ic));
+		varfn = fullfile(out_dir, sprintf('%s_convar_%04d.nii', sub, ic));
+		cd(sub_dirs{i})
 		spm_imcalc(SPM.VResMS,varfn,'i1*vsca',{[],[],[],spm_type('float32'),sprintf('Contrast %d variance', ic)},vsca);
 	end
 end
