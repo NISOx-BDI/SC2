d4fed9848ba8f32b5eddd050a8c613c646c9b3e5
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2020-06-12 17:53:22 +0100

    debug, file is tab seperated

--- src/lib/make_varcopes.m
+++ src/lib/make_varcopes.m
@@ -2,17 +2,17 @@ function make_varcopes(level1_dir)
 
 	out_dir = fullfile(level1_dir, 'subject_varcopes')
 
-    if ~isdir(out_dir)
-        mkdir(out_dir)
-    end
+	if ~isdir(out_dir)
+		mkdir(out_dir)
+	end
 
-    sub_dirs = cellstr(spm_select('FPList',level1_dir, 'dir','sub-*'));   
+	sub_dirs = cellstr(spm_select('FPList',level1_dir, 'dir','sub-*'));   
 
-    for i = 1:numel(sub_dirs)
+	for i = 1:numel(sub_dirs)
 
-    	[~,sub,~] = fileparts(sub_dirs{i});
-    	subject_mat_file = fullfile(level1_dir, sub, 'SPM.mat')
-    	out_name = fullfile()
+		[~,sub,~] = fileparts(sub_dirs{i});
+		subject_mat_file = fullfile(level1_dir, sub, 'SPM.mat')
+		out_name = fullfile()
 
 		ic = 1; % select the contrast index
 		load subject_mat_file
