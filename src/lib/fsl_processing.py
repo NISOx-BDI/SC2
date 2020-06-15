66f01d955690f63550dd2283e9994d85741c327f
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2020-06-15 17:16:38 +0100

    debug

--- src/lib/fsl_processing.py
+++ src/lib/fsl_processing.py
@@ -715,7 +715,7 @@ def fsl_spm_subject_level_files(spm_level1_dir, fsl_level1_dir, fsl_spm_subject_
             number = float(line)
             dof = dof + number
 
-        cmd = "fslmaths " + fsl_dof_path " -bin -mul " + dof + " " + fsl_dof_path
+        cmd = "fslmaths " + fsl_dof_path + " -bin -mul " + dof + " " + fsl_dof_path
         print(cmd)
         check_call(cmd, shell=True)
 
