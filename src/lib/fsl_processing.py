005611742c1f9e20fcde6d45a8ce56d8a7e1eed3
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2020-06-15 14:09:10 +0100

    debug fsl_spm_subject_level_file

--- src/lib/fsl_processing.py
+++ src/lib/fsl_processing.py
@@ -687,11 +687,11 @@ def fsl_spm_subject_level_files(spm_level1_dir, fsl_level1_dir, fsl_spm_subject_
         os.mkdir(os.path.join(fsl_spm_subject_dir, sub))
         copy_tree(os.path.join(sub_dir,'combined.gfeat'), os.path.join(fsl_spm_subject_dir,sub,'combined.gfeat'))
 
-        spm_cope_path = fullfile(os.path.join(spm_level1_dir, sub, 'con_0001.nii'))
-        spm_varcope_path = fullfile(os.path.join(spm_level1_dir, 'varcopes', sub + '_convar_0001.nii'))
+        spm_cope_path = os.path.join(spm_level1_dir, sub, 'con_0001.nii')
+        spm_varcope_path = os.path.join(spm_level1_dir, 'varcopes', sub + '_convar_0001.nii')
 
-        fsl_cope_path = fullfile(os.path.join(fsl_spm_subject_dir,sub,'combined.gfeat','cope1.feat','stats','cope1.nii.gz'))
-        fsl_varcope_path = fullfile(os.path.join(fsl_spm_subject_dir,sub,'combined.gfeat','cope1.feat','stats','varcope1.nii.gz'))
+        fsl_cope_path = os.path.join(fsl_spm_subject_dir,sub,'combined.gfeat','cope1.feat','stats','cope1.nii.gz')
+        fsl_varcope_path = os.path.join(fsl_spm_subject_dir,sub,'combined.gfeat','cope1.feat','stats','varcope1.nii.gz')
 
         # Delete FSL copes and varcopes and replace with SPMs
         os.remove(fsl_cope_path)
