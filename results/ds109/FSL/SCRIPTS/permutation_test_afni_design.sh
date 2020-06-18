c87109079ef12be4dfa8aec2e2272058827f4e7f
Alexander Bowring <alex.bowring@bdi.ox.ac.uk>
2020-06-18 19:25:32 +0100

    FSL ds109 scripts: vanilla FSL, AFNI design, SPM design, AFNI drift, SPM drift, SPM subject-level

---
+++ results/ds109/FSL/SCRIPTS/permutation_test_afni_design.sh
@@ -0,0 +1,6 @@
+cd /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/results/ds109/FSL/LEVEL2_AFNI_DESIGN/permutation_test
+
+fslmerge -t contrasts /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/results/ds109/FSL/LEVEL1_AFNI_DESIGN/sub-*/combined.gfeat/cope1.feat/stats/cope1.nii.gz
+randomise -i contrasts -o OneSampT -d /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/results/ds109/FSL/LEVEL2_AFNI_DESIGN/permutation_test/../group.gfeat/design.mat -t /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/results/ds109/FSL/LEVEL2_AFNI_DESIGN/permutation_test/../group.gfeat/design.con -x -c `ptoz 0.005` -n 10000 -m /well/nichols/users/bas627/BIDS_Data/RESULTS/SC2/results/ds109/FSL/LEVEL2_AFNI_DESIGN/permutation_test/../group.gfeat/cope1.feat/mask.nii.gz
+fslmaths OneSampT_clustere_corrp_tstat1 -thr 0.95 -bin -mul OneSampT_tstat1 05FWECorrected_OneSampT_pos_exc_set
+fslmaths OneSampT_clustere_corrp_tstat2 -thr 0.95 -bin -mul OneSampT_tstat2 05FWECorrected_OneSampT_neg_exc_set
