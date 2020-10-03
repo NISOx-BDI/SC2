Updated colorbars in notebooks to go from 0.5 to 1, also fixed a bug where wrong value was displayed in one entry of correlation tables

figures/lib/correlation_tables.py
@@ -119,7 +119,7 @@ def new_correlation_matrix(df, title):
     ax1 = fig.add_subplot(111)
     cmap = cm.get_cmap('Reds')
     cmap.set_bad('w')
-    cax = ax1.imshow(dfmsk, interpolation="nearest", cmap=cmap, vmin=0, vmax=1)
+    cax = ax1.imshow(dfmsk, interpolation="nearest", cmap=cmap, vmin=0.5, vmax=1)
     
     for (i, j), z in np.ndenumerate(df):
         if (j < i):
@@ -134,7 +134,7 @@ def new_correlation_matrix(df, title):
     ax1.set_xticklabels(labels,fontsize=12)
     ax1.set_yticklabels(labels,fontsize=12)
     # Add colorbar, make sure to specify tick locations to match desired ticklabels
-    fig.colorbar(cax, ticks=[0,1/6,2/6,3/6,4/6,5/6,1], fraction=0.046, pad=0.04)
+    fig.colorbar(cax, ticks=[0.5,7/12,8/12,9/12,10/12,11/12,1], fraction=0.046, pad=0.04)
     ax1.spines['right'].set_visible(False)
     ax1.spines['top'].set_visible(False)
     ax1.yaxis.set_ticks_position('none')
@@ -447,7 +447,7 @@ def new_correlation_tables(stat_file_1, stat_file_2, stat_file_3, stat_file_4, s
     correlation_46,
     correlation_56,
     1,
-    correlation_57
+    correlation_67
     ]
     
     correlation_coefficients[:, 6] = [