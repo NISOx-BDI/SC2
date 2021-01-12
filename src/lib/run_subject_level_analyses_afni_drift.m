d6fbb7303f9579a768247e6dc4df79538b753b0d
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2021-01-12 18:09:03 +0000

    made a start on SPM subject-level analyses with afni drift basis

---
+++ src/lib/run_subject_level_analyses_afni_drift.m
@@ -0,0 +1,20 @@
+function run_subject_level_analyses_afni_drift(sub_template, level1_dir, level1_dir_afni_design, afni_design_dir)
+    
+    if ~isdir(level1_dir_afni_design)
+        mkdir(level1_dir_afni_design)
+    end
+
+    scripts_dir = fullfile(level1_dir, '..', 'SCRIPTS');
+
+    if ~isdir(scripts_dir)
+        mkdir(scripts_dir)
+    end
+
+    sub_dirs = cellstr(spm_select('FPList',level1_dir, 'dir','sub-*'));   
+    
+    for i = 1:numel(sub_dirs)
+        
+        copyfile(sub_dirs{i}, level1_dir_afni_design)
+
+    end
+end
