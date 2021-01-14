2b4631969a691df6f6077bc24cb604db17e861a1
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2021-01-14 17:02:47 +0000

    copying the level1 AFNI design files to level1 AFNI drift

--- src/lib/run_subject_level_analyses_afni_drift.m
+++ src/lib/run_subject_level_analyses_afni_drift.m
@@ -1,7 +1,7 @@
-function run_subject_level_analyses_afni_drift(sub_template, level1_dir_afni_design, level1_dir_afni_drift, afni_design_dir)
+function run_subject_level_analyses_afni_drift(sub_template, level1_dir_afni_design, level1_dir_afni_drift, afni_regressors_dir)
     
-    if ~isdir(level1_dir_afni_design)
-        mkdir(level1_dir_afni_design)
+    if ~isdir(level1_dir_afni_drift)
+        mkdir(level1_dir_afni_drift)
     end
 
     scripts_dir = fullfile(level1_dir, '..', 'SCRIPTS');
@@ -10,11 +10,11 @@ function run_subject_level_analyses_afni_drift(sub_template, level1_dir_afni_des
         mkdir(scripts_dir)
     end
 
-    sub_dirs = cellstr(spm_select('FPList',level1_dir_afni_drift, 'dir','sub-*'));   
+    sub_dirs = cellstr(spm_select('FPList',level1_dir_afni_design, 'dir','sub-*'));   
     
     for i = 1:numel(sub_dirs)
         
-        copyfile(sub_dirs{i}, level1_dir_afni_design)
+        copyfile(sub_dirs{i}, level1_dir_afni_drift);
 
     end
 end
