058a6a387bfe6f5a3a7a5d428912ae7e5c3d39d3
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2021-01-14 18:20:13 +0000

    debug

--- src/lib/run_subject_level_analyses_afni_drift.m
+++ src/lib/run_subject_level_analyses_afni_drift.m
@@ -26,7 +26,7 @@ function run_subject_level_analyses_afni_drift(sub_template, level1_dir_afni_des
         SPM.xX.K(1).X0 = afni_drift_mat;
 
         % Also change the working directory to the new LEVEL1_DRIFT directory
-        SPM.swd = level1_dir_afni_drift;
+        SPM.swd = fullfile(level1_dir_afni_drift, sub);
 
         save(fullfile(level1_dir_afni_drift, sub, 'SPM.mat'), 'SPM');
     end
